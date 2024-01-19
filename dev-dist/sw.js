cd43afaa95c41701bfd1659206ca2655cd3d55b3
nat730 <natanaelcolart@gmail.com>
2024-01-19 09:26:30 +0100

    debut journée

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7hl08ms8jc8"
+    "revision": "0.b34mcj43hj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
