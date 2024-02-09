fb1dd94322f60770b18355aa61ad74e719a78f5c
nat730 <natanaelcolart@gmail.com>
2024-02-09 11:12:40 +0100

    maj front

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cj1nbepejig"
+    "revision": "0.o1c9im1553o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
