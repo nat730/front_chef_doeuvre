a1008109463f712db19688659ba01e8ca2880471
nat730 <natanaelcolart@gmail.com>
2024-02-12 15:16:14 +0100

    ajout de test avec playwright

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,8 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b34mcj43hj8"
-
+    "revision": "0.5iu1kgnsmjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
