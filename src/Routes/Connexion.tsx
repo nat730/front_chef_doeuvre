2dac104bde2355868970b4d99e84c794f7554345
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-12 13:56:46 +0100

    changement intégration page connexion

--- src/Routes/Connexion.tsx
+++ src/Routes/Connexion.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import '../css/globals.css'
 import '../css/connexion.css'
 import { Input } from '@/components/ui/input';
-import { Label } from '@radix-ui/react-label';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardTitle } from '@/components/ui/card';
 
 const Connexion = () => {
+  const navigate = useNavigate();
   const [identifier, setIdentifier] = useState('');
   const [password, setPassword] = useState('');
 
@@ -15,21 +19,65 @@ const Connexion = () => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const handleLogin = useCallback(async() => {
+    const response = await fetch('http://localhost:3000/api/auth/local', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        identifier,
+        password
+      })
+    });
+    const data = await response.json();
+    if(data) {
+      navigate('/')
+    }
+    else {
+      setIdentifier('')
+      setPassword('')
+    }
+  }, [navigate, identifier, password])
+
+  const handleSignInClick = useCallback(() => {
+    navigate('/inscription');
+  }, [])
 
   return (
-    <div className="connexion-container">
-      <h1>Me Connecter</h1>
-      <form className="id-container" onSubmit={handleLogin}>
-        <Label htmlFor="email">Email</Label>
-        <Input type='email' name='email' value={identifier} placeholder='Email' onChange={handleIdentifierChange}></Input>
-        <Label htmlFor='password'>Mot de passe</Label>
-        <Input type='password' name='password' value={identifier} placeholder='Mot de passe' onChange={handlePasswordChange}></Input>
-
-        <button type="submit">Se connecter</button>
-      </form>
+    <div className="cards-container">
+      <Card className="connexion-card">
+        <CardTitle className='text-center'>Drive Solidaire</CardTitle>
+        <CardDescription className='card-description text-center'>Connectez-vous pour accéder à votre espace personnel</CardDescription>
+        <CardContent>
+          <Input
+            type='email'
+            name='email'
+            value={identifier}
+            placeholder='Email'
+            onChange={handleIdentifierChange}
+          />
+          <Input
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Mot de passe'
+            onChange={handlePasswordChange}
+          />
+        </CardContent>
+        <CardFooter
+          className="connexion-card"
+        >
+          <Button
+            type="submit"
+            onClick={handleLogin}
+            className='mb-3'
+          >Me connecter
+          </Button>
+          <p>Pas encore de compte ?</p>
+          <Button variant='ghost' className='font-semibold' onClick={handleSignInClick}>Créer mon compte</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
