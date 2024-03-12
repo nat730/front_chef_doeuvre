73b31e32a425f6b8ec1584797d2cdd5f6240caa2
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-12 10:01:01 +0100

    installation shadcnui et import composant input et label

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -65,4 +67,9 @@ export default defineConfig({
       ]
     },
   })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
 })
