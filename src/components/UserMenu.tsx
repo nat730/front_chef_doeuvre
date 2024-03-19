91ed282ea50b7104c274d75714400005ae526f67
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-19 13:32:54 +0100

    structure et mise en page menu user

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 
-type UserMenuProps = {
+interface UserMenuProps {
   user: string | null;
   toggleUserMenu: () => void;
 };
@@ -8,10 +8,13 @@ type UserMenuProps = {
 const UserMenu = ({user, toggleUserMenu}: UserMenuProps) => {
   return (
     <div className='user-menu'>
+      <div className="left-user-menu">
+        <div className='user-menu-close'>
+          <X size={25} strokeWidth={2} className="close-user-menu" onClick={() => toggleUserMenu()}/>
+        </div>
+      </div>
+      <div className="right-user-menu">
         <div className="user-menu-top-container">
-          <div className='user-menu-close'>
-            <X size={25} strokeWidth={2} className="close-user-menu" onClick={() => toggleUserMenu()}/>
-          </div>
           <div className="user-menu-title-container">
             <h1 className="user-menu-title">Bonjour {user}</h1>
           </div>
@@ -19,17 +22,22 @@ const UserMenu = ({user, toggleUserMenu}: UserMenuProps) => {
         <div className="user-menu-content">
           <div className="user-menu-profil-content">
             <h1>Mon profil</h1>
-            <h2 className='user-menu-secondary-title'>Mon compte</h2>
-            <h2 className='user-menu-secondary-title'>Mes préférences</h2>
-            <h2 className='user-menu-secondary-title'>Mon compte</h2>
+            <div className="user-menu-secondary-title-container">
+              <h2 className='user-menu-secondary-title'>Mon compte</h2>
+              <h2 className='user-menu-secondary-title'>Mes préférences</h2>
+              <h2 className='user-menu-secondary-title'>Mon compte</h2>
+            </div>
           </div>
           <div className="user-menu-orders-content">
             <h1>Mes commandes</h1>
-            <h2 className='user-menu-secondary-title'>Mon historique de commande</h2>
-            <h2 className='user-menu-secondary-title'>Mes commandes en cours</h2>
+            <div className="user-menu-secondary-title-container">
+              <h2 className='user-menu-secondary-title'>Mon historique de commande</h2>
+              <h2 className='user-menu-secondary-title'>Mes commandes en cours</h2>
+            </div>
           </div>
         </div>
       </div>
+    </div>
   )
 };
 
