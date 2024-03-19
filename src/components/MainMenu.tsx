4f416b952f4899af133d3b51bf6578b52e815ef0
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-19 12:00:25 +0100

    ajout liens menu principal

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,10 +1,24 @@
 import { Home, List, MessageCircleQuestion, X } from "lucide-react";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
-type MainMenuProps = {
+interface MainMenuProps {
   toggleMainMenu: () => void;
 }
 
 const MainMenu = ({toggleMainMenu}: MainMenuProps) => {
+
+  const navigate = useNavigate();
+
+  const handleAboutClick = useCallback(
+    () => {
+      navigate('/about');
+    }, [navigate])
+
+  const handleConnexionClick = useCallback(
+    () => {
+      navigate('/connexion');
+    }, [navigate]);
 
   return (
     <div className='main-menu'>
@@ -12,11 +26,11 @@ const MainMenu = ({toggleMainMenu}: MainMenuProps) => {
         <div className="main-menu-top-container">
           <div className="main-menu-title-container">
             <p>Bienvenue sur</p>
-            <h1 className="main-menu-title">Drive solidaire</h1>
+            <h1 className="main-menu-title">Drive Solidaire</h1>
           </div>
         </div>
         <div className="main-menu-content">
-          <div className="home-container">
+          <div className="home-container" onClick={() => toggleMainMenu()}>
             <Home size={25} strokeWidth={2} className="home-icon"/>
             <h2>Accueil</h2>
           </div>
@@ -24,11 +38,13 @@ const MainMenu = ({toggleMainMenu}: MainMenuProps) => {
             <List size={25} strokeWidth={2} className="categories-icon"/>
             <h2>Catégories</h2>
           </div>
-          <div className="about-container">
+          <div className="about-container" onClick={handleAboutClick}>
             <MessageCircleQuestion size={25} strokeWidth={2} className="about-icon"/>
             <h2>Qui sommes-nous ?</h2>
           </div>
-          <h2>Connexion</h2>
+          <div className='connexion-container' onClick={handleConnexionClick}>
+            <h2>Connexion</h2>
+          </div>
         </div>
       </div>
       <div className="right-main-menu" onClick={() => toggleMainMenu()}>
