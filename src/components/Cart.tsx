b73e6bdf9582bf0c71e39fa1425693702de16ad7
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-24 16:02:29 +0100

    fetch génération panier aléatoire

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,37 @@
 import { X } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useCallback, useState } from "react";
 
 interface CartProps {
   toggleCart: () => void;
 }
 
 const Cart = ({toggleCart}: CartProps) => {
+
+  const [cartRandomValue, setCartRandomValue] = useState<number>(0);
+
+  const handleValueChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    setCartRandomValue(newValue);
+  }, []);
+
+  const handleGenerateRandomCart = useCallback(async () => {
+    // il faut ajouter une verif pour s'asssurer que cartRandomValue n'est pas égale à 0 ou est nul
+    try {
+      const response = await fetch('http://localhost:3000/api/cart/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ value: cartRandomValue }),
+    });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }, []);
 
   return (
     <div className='user-menu'>
@@ -20,7 +47,10 @@ const Cart = ({toggleCart}: CartProps) => {
           </div>
         </div>
         <div className="user-menu-content">
-          <div className="user-menu-profil-content"></div>
+          <div className="user-menu-profil-content">
+            <Input placeholder="Entrer une somme" type="number" onChange={handleValueChange} />
+            <Button onClick={handleGenerateRandomCart}>Générer un panier</Button>
+          </div>
       </div>
     </div>
     </div>
