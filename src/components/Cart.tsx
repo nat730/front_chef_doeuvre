17f10e92037e675cb2395c36826e28e37eb3de65
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-25 11:27:23 +0100

    modif

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -34,26 +34,28 @@ const Cart = ({toggleCart}: CartProps) => {
   }, []);
 
   return (
-    <div className='user-menu'>
-      <div className="left-user-menu">
-        <div className='user-menu-close'>
-          <X size={25} strokeWidth={2} className="close-user-menu" onClick={() => toggleCart()}/>
-        </div>
-      </div>
-      <div className="right-user-menu">
-        <div className="user-menu-top-container">
-          <div className="user-menu-title-container">
-            <h1 className="user-menu-title">Mon Panier</h1>
+    <>
+      <div className='user-menu'>
+        <div className="left-user-menu">
+          <div className='user-menu-close'>
+            <X size={25} strokeWidth={2} className="close-user-menu" onClick={() => toggleCart()}/>
           </div>
         </div>
-        <div className="user-menu-content">
-          <div className="user-menu-profil-content">
-            <Input placeholder="Entrer une somme" type="number" onChange={handleValueChange} />
-            <Button onClick={handleGenerateRandomCart}>Générer un panier</Button>
+        <div className="right-user-menu">
+          <div className="user-menu-top-container">
+            <div className="user-menu-title-container">
+              <h1 className="user-menu-title">Mon Panier</h1>
+            </div>
           </div>
+          <div className="user-menu-content">
+            <div className="user-menu-profil-content">
+              <Input placeholder="Entrer une somme" type="number" onChange={handleValueChange} />
+              <Button onClick={handleGenerateRandomCart}>Générer un panier</Button>
+            </div>
+        </div>
       </div>
-    </div>
-    </div>
+      </div>
+    </>
   );
 }
 
