20df214d11d2c9cefe06cc4cd3045b2b311981a0
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-25 13:42:52 +0100

    creation composant ProductCard

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,39 @@
+import { IProduct } from "@/Routes/Accueil";
+import { Card } from "./ui/card";
+import { Button } from "./ui/button";
+
+interface IProductCard {
+  products: IProduct[] | null;
+}
+
+const ProductCard = ({products}: IProductCard) => {
+
+  return (
+    <>
+     <div className='product-category'>
+      {products &&
+        products.map((product, index) => (
+          <Card key={index} className="product-card">
+            {product.CatalogItems !== undefined &&
+              product.CatalogItems.map((item, index) => (
+                <img key={index} src={item.image} alt={product.name} />
+              ))
+            }
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            {product.CatalogItems.map((item, index) => (
+              <p key={index}>{item.price} € / {product.unit_value}</p>
+            ))}
+            <div className='product-cta'>
+              <input type="number" className='input-quantity' name={product.name} />
+              <Button>Ajouter au panier</Button>
+            </div>
+          </Card>
+        ))
+      }
+    </div>
+    </>
+  );
+};
+
+export default ProductCard;
