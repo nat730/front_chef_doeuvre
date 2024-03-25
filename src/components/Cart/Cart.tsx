2693de6bbb081f8d55bcad4c6e8a3042cb1a8ca2
nat730 <natanaelcolart@gmail.com>
2024-03-25 16:56:53 +0100

    application etats globaux

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,45 +1,21 @@
 import './styles.css'
 import { X } from "lucide-react";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
-import { useCallback, useState } from "react";
+import { useStore } from "./Zustand";
+
 
 interface CartProps {
   toggleCart: () => void;
 }
 
-const Cart = ({toggleCart}: CartProps) => {
-
-  const [cartRandomValue, setCartRandomValue] = useState<number>(0);
-
-  const handleValueChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = Number(e.target.value);
-    setCartRandomValue(newValue);
-  }, []);
-
-  const handleGenerateRandomCart = useCallback(async () => {
-    // il faut ajouter une verif pour s'asssurer que cartRandomValue n'est pas égale à 0 ou est nul
-    try {
-      const response = await fetch('http://localhost:3000/api/cart/generate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ value: cartRandomValue }),
-    });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log('error', error);
-    }
-  }, []);
+const Cart = ({ toggleCart }: CartProps) => {
+  const { cartItems } = useStore();
 
   return (
     <>
       <div className='cart-menu'>
         <div className="left-cart-menu">
           <div className='cart-menu-close'>
-            <X size={25} strokeWidth={2} className="close-cart-menu" onClick={() => toggleCart()}/>
+            <X size={25} strokeWidth={2} className="close-cart-menu" onClick={() => toggleCart()} />
           </div>
         </div>
         <div className="right-cart-menu">
@@ -47,14 +23,15 @@ const Cart = ({toggleCart}: CartProps) => {
             <div className="cart-menu-title-container">
               <h1 className="cart-menu-title">Mon Panier</h1>
             </div>
+            <ul>
+              {Object.values(cartItems).map((item) => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+            </ul>
           </div>
-          <div className="cart-menu-content">
-            <div className="cart-menu-profil-content">
-              <Input placeholder="Entrer une somme" type="number" onChange={handleValueChange} />
-              <Button onClick={handleGenerateRandomCart}>Générer un panier</Button>
-            </div>
+          <div className="user-menu-content">
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
