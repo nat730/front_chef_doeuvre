4fd1ddfc56fe3b6e2dc6814c54923404161810fb
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-26 12:30:51 +0100

    ajout état globaux panier non fonctionnel

--- src/Routes/CartValidation.tsx
+++ src/Routes/CartValidation.tsx
@@ -1,10 +1,14 @@
+import { useStore } from "@/components/Cart/Zustand";
 import ShopSteps from "@/components/ShopSteps/ShopSteps";
 import { Button } from "@/components/ui/button";
+import { Minus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+
 
 
 const CartValidation = () => {
 
+  const { cartItems, removeItem } = useStore();
   const navigate = useNavigate();
 
   const handleValidationClick = () => {
@@ -17,6 +21,16 @@ const CartValidation = () => {
       <h1>Détail de votre panier</h1>
       <p><a onClick={() => navigate('/connexion')}>Connectez-vous</a> pour sauvegarder votre panier</p>
       <ShopSteps />
+      <ul>
+        {Object.values(cartItems).map((item) => (
+          <li key={item.id}>
+            {item.name}
+            <button onClick={() => removeItem(item)}>
+            <Minus size={25} strokeWidth={2} />
+            </button>
+          </li>
+        ))}
+            </ul>
       <Button onClick={handleValidationClick}>Valider mon panier</Button>
     </>
   )
