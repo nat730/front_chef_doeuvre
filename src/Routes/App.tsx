2de120c975a00b650f1c593c768169698b98ada8
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-26 16:35:05 +0100

    refactorisation routing ajout composant partagé dans composant Page

--- src/Routes/App.tsx
+++ src/Routes/App.tsx
@@ -1,46 +1,36 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route
+  createBrowserRouter,
+  RouterProvider
 } from "react-router-dom";
 import Connexion from "./Connexion";
 import Accueil from "./Accueil";
 import Inscription from "./Inscription";
 import Page from "../components/Page";
 import UserInfo from "./Profil";
-import Header from "@/components/Header/Header";
 
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Page protectedPage={false} Content={Accueil} />,
-//   },
-//   {
-//     path: "/connexion",
-//     element: <Page protectedPage={false} Content={Connexion} />,
-//   },
-//   {
-//     path: '/inscription',
-//     element: <Page protectedPage={false} Content={Inscription} />
-//   },
-//   {
-//     path: '/user',
-//     element: <Page protectedPage={true} Content={UserInfo} />
-//   }
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Page protectedPage={false} Content={Accueil} />,
+  },
+  {
+    path: "/connexion",
+    element: <Page protectedPage={false} Content={Connexion} />,
+  },
+  {
+    path: '/inscription',
+    element: <Page protectedPage={false} Content={Inscription} />
+  },
+  {
+    path: '/user',
+    element: <Page protectedPage={true} Content={UserInfo} />
+  }
+]);
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Page protectedPage={false} Content={Accueil} />} />
-        <Route path="/connexion" element={<Page protectedPage={false} Content={Connexion} />} />
-        <Route path="/inscription" element={<Page protectedPage={false} Content={Inscription} />} />
-        <Route path="/user" element={<Page protectedPage={true} Content={UserInfo} />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 };
 
