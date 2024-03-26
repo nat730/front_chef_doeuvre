45688ae30f271184ed944b335513d80a97d1f73b
nat730 <natanaelcolart@gmail.com>
2024-03-26 11:54:31 +0100

    changement props pour faciliter explication

--- src/components/Cart/Zustand.tsx
+++ src/components/Cart/Zustand.tsx
@@ -1,20 +1,34 @@
 import {create} from 'zustand';
 
-type CartItem = {
+export type CartItem = {
   id: number;
   name: string;
-  quantity:number
+  price:number;
+  quantity:number;
 };
 
-type Store = {
+type Store = {  
   cartItems: { [id: number]: CartItem };
   addItem: (item: CartItem) => void;
+  removeItem: (item: CartItem) => void;
 };
 
 export const useStore = create<Store>((set) => ({
   cartItems: {},
-  addItem: (item) => set((state) => ({ cartItems: { ...state.cartItems, [item.id]: item } })),
-}));
-
-// Object.keys()
-// Object.values()
+  addItem: (item) => set((state) => {
+    const newCartItems = { ...state.cartItems };
+    console.log(newCartItems,"newcart");
+    
+    if (newCartItems[item.id]) {
+      newCartItems[item.id].quantity += item.quantity;
+    } else {
+      newCartItems[item.id] = item;
+    }
+    return { cartItems: newCartItems };
+  }),
+  removeItem: (item) => set((state) => {
+    const newCartItems = { ...state.cartItems };
+    delete newCartItems[item.id];
+    return { cartItems: newCartItems };
+  }),
+  }));
