695ab1596cfa85c3b9698a50fcb2e21cb82855e1
Zelie Chemin <zelie.chemin@gmail.com>
2024-03-26 12:07:44 +0100

    retrait état global

--- src/Routes/CartValidation.tsx
+++ src/Routes/CartValidation.tsx
@@ -1,4 +1,3 @@
-import { useStore } from "@/components/Cart/Zustand";
 import ShopSteps from "@/components/ShopSteps/ShopSteps";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
@@ -7,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 const CartValidation = () => {
 
   const navigate = useNavigate();
-  const { cartItems } = useStore();
 
   const handleValidationClick = () => {
     navigate('/delivery');
@@ -19,11 +17,6 @@ const CartValidation = () => {
       <h1>Détail de votre panier</h1>
       <p><a onClick={() => navigate('/connexion')}>Connectez-vous</a> pour sauvegarder votre panier</p>
       <ShopSteps />
-      <ul>
-        {Object.values(cartItems).map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
       <Button onClick={handleValidationClick}>Valider mon panier</Button>
     </>
   )
